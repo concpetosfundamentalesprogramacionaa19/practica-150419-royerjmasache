5e937ff5a57ebfb4f96fe9be53fe41752fe0f4e9
royerjmasache <josethxm@gmail.com>
2019-04-15 16:22:31 -0500

    Ejemplos de Python creados en Sublime Text

---
+++ EjemplosPython/Hola.py
@@ -0,0 +1,5 @@
+"""
+	Este es mi primer ejemplo
+	en lenguaje Python
+"""
+print("Hello world")
---
+++ EjemplosPython/Presentacion.py
@@ -0,0 +1,8 @@
+"""
+	Presentaremos el nombre
+	y el apellido de una persona
+"""
+nombre = "Royer"
+apellido = "Masache"
+print(nombre)
+print(apellido)
