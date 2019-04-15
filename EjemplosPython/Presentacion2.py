12a91fc7be1aa65b6d671f76bdea7720d9f7695e
royerjmasache <josethxm@gmail.com>
2019-04-15 16:57:18 -0500

    Ejercicios en Python con Sublime Text

---
+++ EjemplosPython/Presentacion2.py
@@ -0,0 +1,11 @@
+u"""
+	Este es mi tercer ejemplo
+	en el lenguaje Python
+
+	Ingreso de informacion por teclado
+
+	@royerjmasache
+"""
+nombre = input("Por favor ingrese su nombre:")
+apellido = input("Por favor ingrese su apellido:")
+print("Mi nombre es:",nombre, "\n\tMi apellido es:\n",apellido)
